e3260bd040beac87bf6410bcbe61d338a04c7385
Farah <farahalmasri562@gmail.com>
2023-03-19 15:24:12 +0300

    added for Loop

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 let name = prompt("Please enter your name:");
 let gender = prompt("Please enter your gender :");
 
+while (gender !== "male" && gender !== "female") {
+  gender = prompt("Please enter a valid gender (male or female):").toLowerCase();
+}
+
+
 if (gender === "male") {
 alert("Welcome, Mr " + name + "!");
 } else if (gender === "female") {
@@ -14,4 +19,11 @@ let drinkName = prompt("What drink would you like to order?");
 
 alert("Your " + drinkType + " " + drinkName + " is being prepared.");
 
+const userAnswers = [name, gender, drinkType, drinkName];
+
+userAnswers.push("new value");
+
+for (let i = 0; i < userAnswers.length; i++) {
+  console.log(userAnswers[i]);}
+  
 console.log(name + " ordered a " + drinkType + " " + drinkName + ".");
